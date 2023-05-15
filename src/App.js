3ab10475fa59a3e9aaece3b4f2fac9c982621322
Yaksh <yakshrawal@gmail.com>
2023-05-15 15:59:58 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+import Sentiment from 'sentiment';
+const sentiment = new Sentiment();
+
 function App() {
+
+  const [phrase, setPhrase] = useState('');
+  const [sentimentScore, setSentimentScore] = useState(null);
+
+  useEffect(() => {
+    setSentimentScore(sentiment.analyze(phrase));
+  }, [phrase]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Sentiment Analysis</h2>
+
+        <input value={phrase} onChange={e => setPhrase(e.target.value)}
+          style={{ padding: '20px', fontSize: '20px', width: '90%' }}
+        />
+
+        {
+          sentimentScore !== null ?
+            <p>Sentiment Score: {sentimentScore.score}</p>
+            : ''
+        }
+
+        {
+          sentimentScore ?
+            sentimentScore.score === 0 ?
+             '😊😊😊'
+              :
+              sentimentScore.score > 0 ?
+               '😃😃😃'
+                :
+                '😔😔😔'
+            : ''
+        }
+
       </header>
     </div>
   );
